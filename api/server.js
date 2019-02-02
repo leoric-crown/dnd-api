5f0c64c194f3ee72d1762a0260b74c15b7da0dbc
Manuel Quijano <mquijanogv@gmail.com>
2019-02-02 17:03:47 -0600

    (Refactor): Convert server into Class

---
+++ api/server.js
@@ -0,0 +1,61 @@
+require('dotenv').config()
+const cors = require('cors')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const bodyParser = require ('body-parser')
+const mongoose = require ('mongoose')
+const morganBody = require('morgan-body')
+//const morgan = require('morgan')
+//app.use(morgan('dev'))
+const encounterRoutes = require('./routes/encounters')
+const characterRoutes = require('./routes/characters')
+const initiativeRoutes = require('./routes/initiatives')
+const conditionRoutes = require('./routes/conditions')
+const userRoutes = require('./routes/users')
+
+morganBody(app, {logResponseBody: true})
+
+module.exports = class ServerApp {
+    /**
+     * @param  {Object} config single object containing configurations
+     * @return {App}    Server application wrapper
+     */
+    constructor(config, chalk) {
+        this.config = config;
+        this.chalk = chalk
+    }
+    start() {
+      this.app = express();
+      this.app.use(cors());
+      this.app.use(bodyParser.json());
+      this.app.use('/uploads', express.static('uploads'))
+      this.app.use('/users', userRoutes)
+      this.app.use('/encounters', encounterRoutes)
+      this.app.use('/characters', characterRoutes)
+      this.app.use('/initiatives', initiativeRoutes)
+      this.app.use('/conditions', conditionRoutes)
+
+      this.app.use((req, res, next) => {
+        const error = new Error('Resource not found')
+        error.status = 404
+        next(error)
+      })
+
+      this.app.use((error, req, res, next) => {
+        res.status(error.status || 500)
+        res.json({
+          error: {
+            message: error.message
+          }
+        })
+      })
+
+
+      return this.app.listen(5000, () => {
+          console.log(this.chalk.bold.green(`
+              Started Image Express server listening to port 5000
+          `.trim()));
+      });
+    }
+  }
--- app.js
+++ app.js
@@ -1,86 +1,23 @@
+const Server = require('./api/server');
+const mongoose = require ('mongoose')
 require('dotenv').config()
 const config = require('./config/main')
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const bodyParser = require ('body-parser')
-const mongoose = require ('mongoose')
 const mongoConfig = require('./config/mongo')
 const chalk = require('chalk')
-const morganBody = require('morgan-body')
-//const morgan = require('morgan')
-//app.use(morgan('dev'))
-const encounterRoutes = require('./api/routes/encounters')
-const characterRoutes = require('./api/routes/characters')
-const initiativeRoutes = require('./api/routes/initiatives')
-const conditionRoutes = require('./api/routes/conditions')
-const userRoutes = require('./api/routes/users')
 
-morganBody(app, {logResponseBody: true})
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
+new Server(config, chalk).start();
+
+mongoose.connect(
+    config.dbpath,
+    { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false},
+);
+
+mongoose.connection.once('open', function() {
+    console.log(chalk.bold.magenta(`Succesfully Connected to MongoDB at ${config.dbpath}`))
 });
 
-app.use('/uploads', express.static('uploads'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use('/users', userRoutes)
-app.use('/encounters', encounterRoutes)
-app.use('/characters', characterRoutes)
-app.use('/initiatives', initiativeRoutes)
-app.use('/conditions', conditionRoutes)
 
-mongoose.connection
-.once('open', () => {
-  console.log(chalk.bold.green('Successfully connected to MongoDB!'))
-  app.emit('ready')
-})
-.on('disconnect', () => {
-  console.log('mongoose disconnected')
-})
-.on('reconnect', () => {
-  console.log('mongoose reconnected')
-})
-.on('error', err => {
-  console.error(chalk.bold.red('Error connecting to MongoDB: ' + err))
-  server.close()
-})
-console.log(chalk.yellow('Connecting to MongoDB at: '+config.dbpath))
-mongoose.connect(
-  config.dbpath,
-  { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false}
-)
-
-app.on('ready', async () => {
-  console.log(chalk.cyan('Express App is ready.'))
-  await mongoConfig()
-  server.listen(config.port || 5000, () => {
-    console.log(chalk.bold.magenta(`Server listening on: http://${config.host}:${config.port}...`))
-  })
-})
-
-app.use((req, res, next) => {
-  const error = new Error('Resource not found')
-  error.status = 404
-  next(error)
-})
-
-app.use((error, req, res, next) => {
-  res.status(error.status || 500)
-  res.json({
-    error: {
-      message: error.message
-    }
-  })
-})
-
-module.exports = app
+mongoose.connection.on('error', function(err) {
+    console.log(chalk.bold.red(`Error connecting to MongoDB at ${config.dbpath}`))
+    process.exit()
+});
